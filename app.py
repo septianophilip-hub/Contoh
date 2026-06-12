d722a1f68bb0f55053e985bdea2e4e3cee8f98c1
septianophilip-hub <septianophilip@gmail.com>
2026-06-12 10:10:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
-st.title("My First Budget Dashboard")
+st.title("Budget Dashboard")
 
-st.write("Hello Philips")
+budget = 100000000
+actual = 65000000
+
+st.metric(
+    "Remaining Budget",
+    f"Rp {budget-actual:,.0f}"
+)
+
+st.progress(actual/budget)
